86eb816ba9ca96766e5d126caff8a129bb344e85
Namwon Kim <knw1234@gmail.com>
2025-02-28 14:06:38 +0900

    init

---
+++ src/main/java/com/kafka/dlq/DlqApplication.java
@@ -0,0 +1,13 @@
+package com.kafka.dlq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DlqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DlqApplication.class, args);
+	}
+
+}
